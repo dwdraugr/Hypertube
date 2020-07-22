1f16741ef4ddacedfdf1126b30d7c4ed7e1b3cba
somerussianlad <somerussianlad@gmail.com>
2020-07-22 17:18:41 +0300

    fix: deployment scripts modified

--- frontback/docker-deploy.sh
+++ frontback/docker-deploy.sh
@@ -1,1 +1,4 @@
-docker-compose down && docker-compose up -d
+docker-compose down -v && docker-compose up -d
+
+# команда для миграций; нужно обязательно дождаться инициализации БД
+sleep 30s && docker-compose exec app bash -c "php /app/artisan migrate" &> /dev/null &
